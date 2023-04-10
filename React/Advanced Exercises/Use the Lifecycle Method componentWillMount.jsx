27d0b399167f13d4294b15be75b2722276a6dc59
Krasimir Dramaliev <krasipeace@abv.bg>
2023-04-10 19:36:15 +0300

    Add LifeCycles and Callbacks

---
+++ React/Advanced Exercises/Use the Lifecycle Method componentWillMount.jsx
@@ -0,0 +1,11 @@
+class MyComponent extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    componentWillMount() {
+        console.log("componentWillMount");
+    }
+    render() {
+        return <div />
+    }
+};
