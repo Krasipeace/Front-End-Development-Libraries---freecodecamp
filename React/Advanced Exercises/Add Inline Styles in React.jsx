d8684a937e0343e113eff6f1ab2e4eb56db78dbf
Krasimir Dramaliev <krasipeace@abv.bg>
2023-04-10 19:36:42 +0300

    Add HTML and CSS Styling in JSX

---
+++ React/Advanced Exercises/Add Inline Styles in React.jsx
@@ -0,0 +1,8 @@
+const styles = { color: "purple", fontSize: 40, border: "2px solid purple" };
+class Colorful extends React.Component {
+    render() {        
+        return (
+            <div style={styles}>Style Me!</div>
+        );
+    }
+};
