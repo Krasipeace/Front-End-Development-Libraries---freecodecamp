4cebbd7ef32ec83ae2ce51c11167d64152c2100f
Krasimir Dramaliev <krasipeace@abv.bg>
2023-04-11 15:17:48 +0300

    Add arrays and states

---
+++ Redux/Arrays/Use the Spread Operator on Arrays.js
@@ -0,0 +1,18 @@
+const immutableReducer = (state = ['Do not mutate state!'], action) => {
+    switch(action.type) {
+        case 'ADD_TO_DO':
+            return [...state, action.todo];
+        default:
+            return state;
+    }
+};
+  
+const addToDo = (todo) => {
+    return {
+        type: 'ADD_TO_DO',
+        todo
+    }
+}
+  
+const store = Redux.createStore(immutableReducer);
+console.log(store.getState()); // [ 'Do not mutate state!' ]
